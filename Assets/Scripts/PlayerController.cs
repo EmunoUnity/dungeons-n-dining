51f263dd37b4f04f7d620a173e5e3d5bd365f87b
Noah Lemaire <70878529+EmunoUnity@users.noreply.github.com>
2023-02-02 12:01:27 -0500

    Player Movement Revision 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticleInput;
+    public float speed = 5f;
+
+    public bool canbeHit, isDashing, isMoving;
+
+    public MeshRenderer meshRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //BasicPlayerMovement
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticleInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticleInput * Time.deltaTime * speed);
+
+        //Dashing
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isMoving == true ||  && isMoving == true)
+        {
+            StartCoroutine(Dash());
+        }
+
+      
+
+        if ( horizontalInput == 0 && verticleInput == 0)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+
+        if (canbeHit == false)
+        {
+
+        }
+
+        if(isDashing == true)
+        {
+            speed = 15;
+        }
+        else
+        {
+            speed = 5;
+        }
+
+        //Attacking
+
+        
+    }
+
+    IEnumerator Dash()
+    {
+        canbeHit = false;
+        isDashing = true;
+        yield return new WaitForSeconds(1);
+        canbeHit = true;
+        isDashing = false;
+    }
+}
