fa86a67f18c1a520ca8079de0da394958a4e9d26
Insanitori <natoriwolf2p@gmail.com>
2023-02-15 12:43:30 -0500

    Requesting Work

--- Assets/ToriScripts/SpawnerScript.cs
+++ Assets/ToriScripts/SpawnerScript.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject Spawner;
-    public int x;
+    //public int x;
     private int y;
+    public bool spawn;
     // Start is called before the first frame update
     void Start()
     {
-        x = 0;
+        //x = 0;
         y = 1;
+
+        spawn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(x == y)
+        if(spawn)
         {
             GameObject gameObject = Instantiate(Spawner, transform.position, transform.rotation);
-            gameObject.name = gameObject.name + " " + y;
+            gameObject.name = "Monster " + y;
             y++;
+            spawn = false;
         }
     }
 }
