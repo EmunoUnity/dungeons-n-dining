cb651ff0e7a062495d4c343ae7b93120d4761027
Insanitori <natoriwolf2p@gmail.com>
2023-02-16 11:16:50 -0500

    So fresh So clean

---
+++ Assets/ToriScripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+
+    private int dangerRange;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        dangerRange = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, player.transform.position) <= dangerRange)
+        {
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
