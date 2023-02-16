fd3132684c0cf9f20fae506a1ac26fc7685efabb
Insanitori <natoriwolf2p@gmail.com>
2023-02-16 10:27:29 -0500

    Temporary movement

---
+++ Assets/ToriScripts/TempMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMovement : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticleInput;
+    public float speed = 5f;
+
+    public bool canbeHit, isDashing, isMoving;
+
+    public MeshRenderer meshRend;
+
+    public GameObject test;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //BasicPlayerMovement
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticleInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * verticleInput * Time.deltaTime * speed);
+
+        //Dashing
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isMoving == true)
+        {
+            StartCoroutine(Dash());
+        }
+
+
+
+        if (horizontalInput == 0 && verticleInput == 0)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+
+        if (canbeHit == false)
+        {
+
+        }
+
+        if (isDashing == true)
+        {
+            speed = 15;
+        }
+        else
+        {
+            speed = 5;
+        }
+
+        //Attacking
+
+
+    }
+
+    IEnumerator Dash()
+    {
+        canbeHit = false;
+        isDashing = true;
+        yield return new WaitForSeconds(1);
+        canbeHit = true;
+        isDashing = false;
+    }
+}
