31b36ed7e95205d01914f6f73b2f4d4d4aed8010
Insanitori <natoriwolf2p@gmail.com>
2023-03-16 03:57:26 -0400

    They found the Drugs: Drown a Man

--- Assets/ToriScripts/enemiesAttackers.cs
+++ Assets/ToriScripts/enemiesAttackers.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class enemiesAttackers : MonoBehaviour
 {
     private Heart hitsss;
+    private PlayKeys playKeys;
     // Start is called before the first frame update
     void Start()
     {
         hitsss = FindObjectOfType<Heart>();
+        playKeys = FindObjectOfType<PlayKeys>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,7 @@ public class enemiesAttackers : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && playKeys.canbeHit1 == true)
         {
             Debug.Log("I have hit the player!!");
             hitsss.myHeath -= 1;
