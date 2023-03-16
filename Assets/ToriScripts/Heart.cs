26e27a2321430ff6006670e46f2e4682a56ab556
Insanitori <natoriwolf2p@gmail.com>
2023-03-16 02:25:08 -0400

    Boink Bitch: Shot a man

---
+++ Assets/ToriScripts/Heart.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    public int myHeath;
+    public int Harts;
+    public Image[] Heahht;
+    public Sprite full;
+    public Sprite empty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        heartsystem();
+
+        working();
+    }
+
+    public void heartsystem()
+    {
+        if(myHeath >= Harts)
+        {
+            myHeath = Harts;
+        }
+
+        if(myHeath <= 0)
+        {
+            myHeath = 0;
+        }
+
+        for (int i = 0; i < Heahht.Length; i++)
+        {
+            if(i < myHeath)
+            {
+                Heahht[i].sprite = full;
+            }
+            else
+            {
+                Heahht[i].sprite = empty;
+            }
+
+            if (i < Harts)
+            {
+                Heahht[i].enabled = true;
+            }
+            else
+            {
+                Heahht[i].enabled = false;
+            }
+        }
+        
+    }
+
+    public void working()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            myHeath -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            myHeath += 1;
+        }
+    }
+}
