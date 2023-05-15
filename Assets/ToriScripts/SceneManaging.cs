33edb4d9e20d21b4c2f067f28cb3ba5b4d267430
Insanitori <natoriwolf2p@gmail.com>
2023-05-14 21:08:26 -0400

    Happy Milf Day

---
+++ Assets/ToriScripts/SceneManaging.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManaging: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void MainGame()
+    {
+        SceneManager.LoadScene("Exportable");
+    }
+
+    public void Win() 
+    {
+        SceneManager.LoadScene("win");
+    }
+
+    public void Lose()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+}
